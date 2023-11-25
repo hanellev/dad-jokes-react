a2b5f8fb4bc1df82db56c627a26274e7a8a16408
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-11-25 17:07:41 +0100

    Vytvoření modrého boxu s funkčními buttony a ikonky palců

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,53 @@
 import './style.css';
+import { useState } from 'react';
+import { jokes } from '../..';
 
 export const HomePage = () => {
-  return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
-  );
+	const [upLikes, setUpLikes] = useState(0);
+	const [downLikes, setDownLikes] = useState(0);
+
+	const handleUpClick = () => {
+		setUpLikes(upLikes + 1);
+	};
+
+	const handleDownClick = () => {
+		setDownLikes(downLikes + 1);
+	};
+
+	return (
+		<div className="container">
+			<div className="joke">
+				<div className="joke__body">
+					<div className="joke__user">
+						<div className="user-avatar"></div>
+						<div className="user-name"></div>
+					</div>
+					<div className="joke__text">
+						<button
+							className="btn-like btn-like--up"
+							onClick={handleUpClick}
+						></button>
+						<span
+							className={`likes-count likes-count--up ${
+								upLikes > 0 ? 'active' : ''
+							}`}
+						>
+							{upLikes}
+						</span>
+						<button
+							className="btn-like btn-like--down"
+							onClick={handleDownClick}
+						></button>
+						<span
+							className={`likes-count likes-count--down ${
+								downLikes > 0 ? 'active' : ''
+							}`}
+						>
+							{downLikes}
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
